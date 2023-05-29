ee1e910ba1c3fcd14644212a50feabe5563e6ea4
Cotamor <cotamor@gmail.com>
2023-05-30 02:21:18 +0900

    Rename api to backend

--- backend/routes/uploadRoute.js
+++ backend/routes/uploadRoute.js
@@ -6,7 +6,7 @@ const router = express.Router()
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'api/uploads')
+    cb(null, 'backend/uploads')
   },
   filename: function (req, file, cb) {
     cb(
