68a7af4581734868cdb864273cfe768f0e392805
Cotamor <cotamor@gmail.com>
2023-05-29 15:56:55 +0900

    Add custom domain

--- api/server.js
+++ api/server.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://edapro.onrender.com'],
+    origin: ['http://localhost:3000', 'https://edapro.onrender.com', 'https://www.edasaki.net'],
     credentials: true,
   })
 )
